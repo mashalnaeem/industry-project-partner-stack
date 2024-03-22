0678a1bb14e6d20266fe5161ef9476c0c24eaa39
Oluwayemisi Bakare <tosine727@gmail.com>
2024-03-22 12:59:24 -0400

    updated detailed page

--- src/pages/DetailsPage/DetailsPage.js
+++ src/pages/DetailsPage/DetailsPage.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import baseUrl from '../../consts'
+import detaillogo from "../../assets/detailed img.svg"
 import './DetailsPage.scss'
 
 function DetailsPage() {
@@ -24,14 +25,26 @@ function DetailsPage() {
 
         , [id])
         //podcasterData is an object now accessible
+        if (!podcasterData) {
+            return (
+            <div>Loading...</div>
+        )
+        }
 
     return (
-        <div className='details'>
-            DetailsPage
-
-           <div><p> {podcasterData.channel}</p></div> 
+        <div className="details__container">
+            <img src={detaillogo} alt="detail-logo"/>
+        <div className="details__container-wrapper">
+        <h2 className='details__container-title'>DetailsPage</h2>
+        <p className='details__container-channel'>{podcasterData.channel}</p>
+        <p className='details__container-channelviewcount'>{podcasterData.channelViewCount}</p>
+        <p className='details__container-subscribercount'>{podcasterData.subscriberCount}</p>
+        <p className='details__container-country'>{podcasterData.country}</p>
         </div>
+        </div>
+    
     )
 }
 
+    
 export default DetailsPage;
